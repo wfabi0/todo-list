6f85fd353df04745ffced7eba5cd14a3e0051d17
wfabi0 <fabioemanuellmilagres@gmail.com>
2024-05-13 17:53:50 -0300

    feat: Add FormConfirmButton component for form submission

---
+++ src/components/form-confirm-button.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useFormStatus } from "react-dom";
+import { UseFormReturn } from "react-hook-form";
+
+type FormConfirmButtonProps = {
+  text?: string;
+  className?: string;
+  open?: boolean;
+  setOpen?: (open: boolean) => void;
+  form?: UseFormReturn<any>;
+};
+
+export default function FormConfirmButton({
+  text,
+  className,
+  open,
+  setOpen,
+  form,
+}: FormConfirmButtonProps) {
+  const { pending } = useFormStatus();
+  text = text || "Continue";
+  className = className || "";
+  if (form) {
+    const { formState } = form;
+    return (
+      <Button
+        onClick={(e) => setOpen && setOpen(!open)}
+        type="submit"
+        disabled={pending}
+        className={`${className}`}
+      >
+        {formState.isLoading ? "Wait..." : text}
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        onClick={(e) => setOpen && setOpen(!open)}
+        type="submit"
+        disabled={pending}
+        className={`${className}`}
+      >
+        {pending ? "Wait..." : text}
+      </Button>
+    );
+  }
+}
